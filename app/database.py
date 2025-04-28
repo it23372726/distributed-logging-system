0ef652357ba107db521126948a595fa61af91dbe
it23372726 <it23372726@my.sliit.lk>
2025-04-28 16:35:59 +0530

    primary_server and stand_server

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Database URL
-SQLALCHEMY_DATABASE_URL = "postgresql://akilaudara@localhost/distributed_logging_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://akilaudara@localhost:5542/distributed_logging_db"
 
 # Create engine
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
